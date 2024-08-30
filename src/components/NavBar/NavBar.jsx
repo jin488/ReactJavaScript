299a84f5510a399c140165ef61c7048e4d4874f3
Juan Ignacio Napoli <148847617+jin488@users.noreply.github.com>
2024-08-30 12:45:20 -0300

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import CartWidget from "./CartWidget"
-import "./navbar.css"
+import "./Navbar.css"
 import { Link } from "react-router-dom";
 import { FaComputer } from "react-icons/fa6";
 import { MdSupportAgent } from "react-icons/md";

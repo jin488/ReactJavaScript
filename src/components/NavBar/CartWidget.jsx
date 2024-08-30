113e461568b2a7474aef81f1dff5b558fb4631e4
Juan Ignacio Napoli <148847617+jin488@users.noreply.github.com>
2024-08-30 12:50:22 -0300

    Update CartWidget.jsx

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,6 +1,6 @@
 import { TiShoppingCart } from "react-icons/ti";
 import { useContext } from "react";
-import { CartContext } from "../../Context/CartContext";
+import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import "./navbar.css"
 

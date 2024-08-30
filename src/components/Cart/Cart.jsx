240c64cdeb6b4a31b9e86c19f3139cf9d4409934
Juan Ignacio Napoli <148847617+jin488@users.noreply.github.com>
2024-08-30 12:47:43 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { CartContext } from "../../Context/CartContext"
+import { CartContext } from "../../context/CartContext"
 import { FaRegTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Cart.css"
